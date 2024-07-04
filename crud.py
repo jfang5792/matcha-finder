79a6dc34c663fd83e1a4a7c5da886a83a656c965
Jenny Fang <jfang5792@gmail.com>
2024-07-03 21:42:33 -0700

    Functions for Users, Places, and Ratings completed. Continue to add more and finish Favorite

--- crud.py
+++ crud.py
@@ -7,22 +7,62 @@ def create_user(email, user_name):
     user = User(email=email, user_name=user_name)
     return user
 
+def get_users():
+    """Returns all users"""
+    return User.query.all()
+
+def get_user_by_id(user_id):
+    """Return a user by primary key."""
+    return User.query.get(user_id)
+
+def get_user_by_email(email):
+    """Return user by email"""
+    return User.query.filter(User.email == email).first()
+
+
+
 def create_place(name, description, website, address):
     """Create a place/generate place result"""
     place = Place(name=name, description=description,
                    website=website, address=address)
     return place
 
-def create_rating(comment, stars, user, place):
-    """Create a rating on a place"""
-    rating = Rating(comment=comment, stars=stars,
-                    user=user, place=place)
+def get_places():
+    """"Return all places"""
+    return Place.query.all()
+
+def get_place_by_id(place_id):
+    """Return a place by primary key."""
+    return Place.query.get(place_id)
+
+
+
+def create_rating(stars, user, place):
+    """Create and return a rating on a place"""
+    rating = Rating(stars=stars, user=user, place=place)
     return rating
+
+def get_ratings():
+    """Return all ratings"""
+    return Rating.query.all()
+
+def update_rating(rating_id, new_star_rating):
+    """Update an existing rating through its rating_id"""
+    rating = Rating.query.get(rating_id)
+    rating.stars = new_star_rating
+
+
+
 
 def create_favorite(user, place):
     """Add a place to favorite"""
     favorite = Favorite(user=user, place=place)
     return favorite
+
+def get_favorites():
+    """Return all favorites"""
+    return Favorite.query.all()
+
 
 
 if __name__ == '__main__':
