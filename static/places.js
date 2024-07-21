98f3c6eb3f3b961f4a785241bc1f8fea8dfc331a
Jenny Fang <jfang5792@gmail.com>
2024-07-20 21:19:56 -0700

    Continue to work on fetch get request

--- static/places.js
+++ static/places.js
@@ -6,7 +6,7 @@ const formSubmit = document.getElementById("search-bar-form").addEventListener("
   console.log(searchInput)
 
   // AJAX request to google places api with the search input added into the request payload
-  fetch('https://maps.googleapis.com/maps/api/place/textsearch/json?query=123%20main%20street&key=AIzaSyA3PXYmFgmH5VgQMt7Vfni7MQS_nISiUsQ')
+  fetch('https://maps.googleapis.com/maps/api/place/textsearch/json?query=123%20main%20street&key={API_KEY}')
     .then((resp) => {
     return resp.json();
     // .then(res => res.json())
