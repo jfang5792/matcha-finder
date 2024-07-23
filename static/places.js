8e21799d4313a69432e5405bc5500eb9c1a4cb8b
Jenny Fang <jfang5792@gmail.com>
2024-07-22 20:27:33 -0700

    fetch call working, now need to print search values in results html

--- static/places.js
+++ static/places.js
@@ -1,14 +1,16 @@
+// const { urlencoded } = require("express");
+
 console.log('js is working')
 
 const formSubmit = document.getElementById("search-bar-form").addEventListener("submit", (e) => {
   e.preventDefault();
   const searchInput = document.getElementById("user-search-input").value;
-  console.log(searchInput)
+  const encodedSearchInput = encodeURI(searchInput)
 
   // AJAX request to google places api with the search input added into the request payload
-  fetch('https://maps.googleapis.com/maps/api/place/textsearch/json?query=123%20main%20street&key={API_KEY}')
-    .then((resp) => {
-    return resp.json();
+  fetch(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=${encodedSearchInput}&key={API_KEY}`)
+    .then((res) => {
+    return res.json();
     // .then(res => res.json())
     // .then(res => res.places);
     })
