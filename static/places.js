aabd03ac2348a238c181471c11b7347c045fc22c
Jenny Fang <jfang5792@gmail.com>
2024-07-25 21:03:46 -0700

    Add cb func and event listener to each favorite button

--- static/places.js
+++ static/places.js
@@ -9,9 +9,28 @@ const formSubmit = document.getElementById("search-bar-form").addEventListener("
 
   // AJAX request to google places api with the search input added into the request payload
   fetch(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=${encodedSearchInput}&key={API_KEY}`)
-    .then((res) => {
-    return res.json();
-    // .then(res => res.json())
-    // .then(res => res.places);
+    .then((res) => {return res.json()})
+    .then((data) => {
+      console.log({data});
+      const resultNode = document.getElementById("search-results-list");
+      const results = data.results;
+      console.log({results});
+
+      results.slice(0, 3).forEach(result => {
+          const liElement= document.createElement("li");
+          liElement.className = "result-item";
+          liElement.innerHTML = `
+              ${result.name}
+              <p>${result.formatted_address}</p>
+              <p><button type="button" class="favorite-button" id=${result.place_id}> Add to Favorite </button></p>
+          `;
+          resultNode.appendChild(liElement);
+      })
+
+    document.querySelector(".favorite-button").addEventListener("submit", (evt) => {
+
     })
+
+    })
+    // .catch((error) => {console.log("No results found, try again")})
 })
