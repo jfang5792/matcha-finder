80554a45051b34b2be3a2731eb23c8f8d09d6cfe
Jenny Fang <jfang5792@gmail.com>
2024-08-01 21:26:46 -0700

    Completed create_place and create_favorite crud functions"

--- crud.py
+++ crud.py
@@ -2,14 +2,12 @@
 
 from model import db, User, Place, Rating, Favorite, connect_to_db
 
+#---------------------------------------------------------------------#
+
 def create_user(email, password):
     """Create new user"""
     user = User(email=email, password=password)
     return user
-
-def get_users():
-    """Returns all users"""
-    return User.query.all()
 
 def get_user_by_id(user_id):
     """Return a user by primary key."""
@@ -19,10 +17,17 @@ def get_user_by_email(email):
     """Return user by email"""
     return User.query.filter(User.email == email).first()
 
-def create_place(name, description, website, address):
+#---------------------------------------------------------------------#
+
+def create_place(name, description, website, address, external_id):
     """Create a place/generate place result"""
-    place = Place(name=name, description=description,
-                   website=website, address=address)
+    # check if place already exists in the database
+        # if it doesn't exist, create it, and then grab the place object id
+        # if exists, get the place object
+    place = Place.query.filter_by(external_id=external_id).first()
+
+    if not place:
+        place = Place(name=name, description=description, website=website, address=address, external_id=external_id)
     return place
 
 def get_places():
@@ -33,7 +38,7 @@ def get_place_by_id(place_id):
     """Return a place by primary key."""
     return Place.query.get(place_id)
 
-
+#---------------------------------------------------------------------#
 
 def create_rating(stars, user, place):
     """Create and return a rating on a place"""
@@ -49,28 +54,26 @@ def update_rating(rating_id, new_star_rating):
     rating = Rating.query.get(rating_id)
     rating.stars = new_star_rating
 
-
-
+#---------------------------------------------------------------------#
 
 def create_favorite(user, place):
-    """Add a place to favorite"""
-    favorite = Favorite(user=user, place=place)
+    """Create a new favorite place"""
+    favorite = Favorite.query.filter_by(user=user, place=place)
+
+    if not favorite:
+        favorite = Favorite(user=user, place=place)
     return favorite
 
-def user_fav(user, place, fav):
-    """Return user's favorite place"""
-    favorited = Favorite(user=user, place=place, fav=fav)
-    return favorited
+def get_favorite_by_user_id(user_id):
+    """Return favorite by user id"""
+    return Favorite.query.get(user_id)
 
-def get_favorites():
-    """Return all favorites"""
-    return Favorite.query.all()
+#---------------------------------------------------------------------#
 
-# def get_favorite_by_user_id(user_id):
-#     """Return favorite by user id"""
-#     return Favorite.query.get(user_id)
-
-
+def delete_place_from_favorites(favorite):
+    """Delete a favorite place."""
+    db.session.delete(favorite)
+    db.session.commit()
 
 if __name__ == '__main__':
     from server import app
