1a2dc1e62b6afaeecf0279dab9700ca32e259573
Jenny Fang <jfang5792@gmail.com>
2024-08-14 20:58:41 -0700

    continue to isolate 3 items from result data

--- frontend/src/Places.jsx
+++ frontend/src/Places.jsx
@@ -1,9 +1,44 @@
-
+import { useState, useEffect} from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 export default function Places() {
+    const location = useLocation();
+    const [results, setResults] = useState([]);
+    useEffect(() => {
+        setResults(location.state);
+    }, [location])
+
+    console.log({results});
+
+    // const navigate = useNavigate();
+    // const viewPlaces = (evt) => {
+    //     evt.preventDefault()
+    //     fetch(`/api/favorites`, {
+    //         method: "POST",
+    //         body: JSON.stringify({
+    //             place_id: evt.target.id
+    //         }),
+    //         headers: {"Content-Type": "application/json"},
+    //     })
+    //     .then((res) => {return res.json()})
+    //     .then((data) => {
+
+    // })
+
+
+    // const viewEachPlace = (evt) => {
+
+    // }
+
+    // }
+
     return (
         <div>
             <h1>List of Matcha spots:</h1>
+            <div>
+                {results}
+
+            </div>
         </div>
     )
 }
