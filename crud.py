470293137ceec6422eceb34a68155b3e78d1f0bc
Jenny Fang <jfang5792@gmail.com>
2024-08-15 11:30:46 -0700

    updated some crud operations - still pending

--- crud.py
+++ crud.py
@@ -2,22 +2,39 @@
 
 from model import db, User, Place, Rating, Favorite, connect_to_db
 
-#---------------------------------------------------------------------#
+#------------------------------------------------------------------------#
+# USERS
 
 def create_user(email, password):
     """Create new user"""
-    user = User(email=email, password=password)
+    user = User(
+        email=email,
+        password=password
+    )
+    db.session.add(user)
+    db.session.commit()
     return user
 
 def get_user_by_id(user_id):
     """Return a user by primary key."""
     return User.query.get(user_id)
 
-def get_user_by_email(email):
-    """Return user by email"""
-    return User.query.filter(User.email == email).first()
+def delete_user_by_email(email):
+    """Delete user from database by email"""
+    user = User.query.get(email)
+    db.session.delete(user)
+    db.session.commit()
+    return
 
-#---------------------------------------------------------------------#
+def delete_user_by_user_id(user_id):
+    """Delete user from database by user id."""
+    user = User.query.get(user_id)
+    db.session.delete(user)
+    db.session.commit()
+    return
+
+#------------------------------------------------------------------------#
+# PLACES
 
 def create_place(name, description, website, address, external_id):
     """Create a place/generate place result"""
@@ -27,7 +44,15 @@ def create_place(name, description, website, address, external_id):
     place = Place.query.filter_by(external_id=external_id).first()
 
     if not place:
-        place = Place(name=name, description=description, website=website, address=address, external_id=external_id)
+        place = Place(
+            name=name,
+            description=description,
+            website=website,
+            address=address,
+            external_id=external_id
+        )
+    # db.session.add(place)
+    # db.session.commit()
     return place
 
 def get_places():
@@ -38,7 +63,21 @@ def get_place_by_id(place_id):
     """Return a place by primary key."""
     return Place.query.get(place_id)
 
-#---------------------------------------------------------------------#
+def get_place_by_external_id(external_id):
+    """Return a place by API external_id."""
+    return Place.query.get(external_id)
+
+def delete_place(user_id, place_id):
+    """Delete place"""
+    place_to_delete = Place.query.filter_by(user_id=user_id, place_id=place_id).first()
+    print(f"Place deleted:", place_to_delete)
+    db.session.delete(place_to_delete)
+    db.session.commit()
+    print(f"Place {place_id} removed from User {user_id} Place()")
+    return
+
+#------------------------------------------------------------------------#
+# RATINGS
 
 def create_rating(stars, user, place):
     """Create and return a rating on a place"""
@@ -54,27 +93,36 @@ def update_rating(rating_id, new_star_rating):
     rating = Rating.query.get(rating_id)
     rating.stars = new_star_rating
 
-#---------------------------------------------------------------------#
+#------------------------------------------------------------------------#
+# FAVORITES
 
 def create_favorite(user, place):
     """Create a new favorite place"""
-    favorite = Favorite.query.filter_by(user=user, place=place)
+    favorite = Favorite.query.filter_by(user=user, place=place).first()
 
     if not favorite:
-        favorite = Favorite(user=user, place=place)
+        favorite = Favorite(
+            user=user,
+            place=place
+        )
     return favorite
 
 def get_favorite_by_user_id(user_id):
     """Return favorite by user id"""
     return Favorite.query.get(user_id)
 
-#---------------------------------------------------------------------#
 
-def delete_place_from_favorites(favorite):
-    """Delete a favorite place."""
-    db.session.delete(favorite)
-    db.session.commit()
+# def delete_place_from_favorites(user_id, favorite_id):
+#     """Delete a favorite place."""
+#     favorite_place_to_delete = Favorite.query.filter_by(user_id=user_id, favorite_id=favorite_id).first()
+#     print(f"Place now removed from Favorites list:", favorite_place_to_delete)
+#     db.session.delete(favorite_place_to_delete)
+#     db.session.commit()
+#     return
 
+
+#------------------------------------------------------------------------#
 if __name__ == '__main__':
     from server import app
     connect_to_db(app)
+    #db.create_all()
