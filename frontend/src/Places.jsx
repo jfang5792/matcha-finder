c5041dcb01e8d317d42205d63f8ebc2f2e3caec0
Jenny Fang <jfang5792@gmail.com>
2024-08-18 20:25:09 -0700

    added MatchaList props

--- frontend/src/Places.jsx
+++ frontend/src/Places.jsx
@@ -1,44 +1,54 @@
 import { useState, useEffect} from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
+import MatchaList from './MatchaList';
+import FavoritesButton from './FavoritesButton';
+import ViewFavoritesPage from './Favorites';
 
 export default function Places() {
     const location = useLocation();
     const [results, setResults] = useState([]);
+    const [searchInput, setSearchInput] = useState("");
     useEffect(() => {
-        setResults(location.state);
-    }, [location])
+        if(location.state.searchInput !== undefined) {
+            setSearchInput(location.state.searchInput)
+            }
+        }, [location])
+        // console.log("LOCATION STATE:", location.state)
+    // console.log({searchInput});
 
-    console.log({results});
-
-    // const navigate = useNavigate();
-    // const viewPlaces = (evt) => {
-    //     evt.preventDefault()
-    //     fetch(`/api/favorites`, {
-    //         method: "POST",
-    //         body: JSON.stringify({
-    //             place_id: evt.target.id
-    //         }),
-    //         headers: {"Content-Type": "application/json"},
-    //     })
-    //     .then((res) => {return res.json()})
-    //     .then((data) => {
-
-    // })
-
-
-    // const viewEachPlace = (evt) => {
-
-    // }
-
-    // }
+    useEffect(() => {
+        if(searchInput === "") {
+            return
+        }
+        const encodedSearchInput = encodeURI(searchInput);
+        // Make API call
+        fetch(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=${encodedSearchInput}&key=API_KEY`)
+        .then((res) => {
+            return res.json()
+        })
+        .then((data) => {
+            // Get first 3 results
+            console.log("DATA:", data)
+            const placeResults = data.results.slice(0, 3);
+            // console.log(placeResults)
+            setResults(placeResults)
+        })
+    }, [searchInput])
 
     return (
         <div>
-            <h1>List of Matcha spots:</h1>
-            <div>
-                {results}
-
-            </div>
+            <MatchaList data={results} addPlacesFavButton={true}/>
         </div>
     )
 }
+
+// import Card from 'react-bootstrap/Card';
+// import { CardBody } from 'react-bootstrap';
+// {/* <Card style={{ width: '18rem' }}>
+
+//             <CardBody> */}
+//             {/* <MatchaList data={data.results}/> */}
+//             {/* </CardBody> */}
+//             {/* <CardBody><MatchaList data={data.results}/></CardBody>
+//             <CardBody><MatchaList data={data.results}/></CardBody> */}
+//             {/* </Card> */}
