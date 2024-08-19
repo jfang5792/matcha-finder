ee15cf9c4a61487dd9e269bcf8bfad26dfca0d8e
Jenny Fang <jfang5792@gmail.com>
2024-08-18 20:26:15 -0700

    included new args in default function

--- frontend/src/MatchaList.jsx
+++ frontend/src/MatchaList.jsx
@@ -1,21 +1,22 @@
 import FavoritesButton from "./FavoritesButton"
+import ViewFavoritesPage from "./Favorites"
 
-
-export default function MatchaList(props) {
-
-    const listItems = props.data.map((listItem) => {
+export default function MatchaList({data, addPlacesFavButton}) {
+    const listItems = data.map((listItem) => {
         return (
             <li key={listItem.name}>
                 {listItem.name}
                 <div>{listItem.formatted_address}</div>
                 <div>{listItem.description}</div>
-                <FavoritesButton placeId={ listItem.place_id }/>
+                { addPlacesFavButton && <FavoritesButton /> }
+                {/* <FavoritesButton placeId={ listItem.place_id }/> */}
             </li>
         )
     })
+
     return (
         <div>
-            <h1>List of Matcha spots:</h1>
+            <h3>List of Matcha spots:</h3>
             <div>
                 <ul>{listItems}</ul>
             </div>
