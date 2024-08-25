6646971188c8db07d7ad253d012fcfaa6adfdb2b
Jenny Fang <jfang5792@gmail.com>
2024-08-25 00:05:50 -0700

    Design each place result into cards using Bootstrap import

--- frontend/src/MatchaList.jsx
+++ frontend/src/MatchaList.jsx
@@ -1,21 +1,33 @@
-import FavoritesButton from "./FavoritesButton"
-import ViewFavoritesPage from "./Favorites"
+import FavoritesButton from "./FavoritesButton";
+import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/CardGroup';
+import vessel from './assets/vessel.jpg';
+// import Row from 'react-bootstrap/Row';
+
 
 export default function MatchaList({data, addPlacesFavButton}) {
     const listItems = data.map((listItem) => {
         return (
-            <li key={listItem.name}>
+            <ul key={listItem.name}>
+                {/* <Row xs={1} md={2} className="g-4"> */}
+                <CardGroup className="placeCards" style={{ maxWidth: '18rem', margin: '0 10px' }}>
+                    <Card>
+                        <Card.Img variant="top" src={vessel}/>
+                        <Card.Body>
                 {listItem.name}
                 <div>{listItem.formatted_address}</div>
                 <div>{listItem.description}</div>
                 {addPlacesFavButton && <FavoritesButton placeId={ listItem.place_id }/>}
-            </li>
+                        </Card.Body>
+                    </Card>
+                </CardGroup>
+                {/* </Row> */}
+            </ul>
         )
     })
-
     return (
         <div>
-            <h3>List of Matcha spots:</h3>
+            <ul><h3>List of Matcha spots:</h3></ul>
             <div>
                 <ul>{listItems}</ul>
             </div>
